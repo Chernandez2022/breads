1e36b6e79c78c97cc400ac09a0ad3f9492892251
Chernandez2022 <hcarlos0816@gmail.com>
2022-08-10 20:38:32 -0400

    was able to render page

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,14 +2,17 @@ const express = require("express");
 const breads = express.Router();
 const Bread = require("../models/bread.js");
 
-// Index
+// INDEX
 breads.get("/", (req, res) => {
-  res.send(Bread);
+  res.render("Index", {
+    breads: Bread,
+    title: "Index Page",
+  });
 });
-
-module.exports = breads;
 
 // SHOW
 breads.get("/:arrayIndex", (req, res) => {
   res.send(Bread[req.params.arrayIndex]);
 });
+
+module.exports = breads;
